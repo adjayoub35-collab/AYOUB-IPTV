0afc42c7a8478328c5257bce323a85038847af68
adjayoub35-collab <adjayoub35@gmail.com>
2026-09-23 09:27:35 +0100

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,19 @@
-plugins {
-    id("com.android.application") version "8.7.3" apply false
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.ayoub.iptv"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.ayoub.iptv"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.1-phone-tv"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
